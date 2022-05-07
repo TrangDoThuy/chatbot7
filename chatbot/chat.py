f4613c2c5980cc5961bbce9919594424b730969a
Emma Thuy Trang DO <30380242+TrangDoThuy@users.noreply.github.com>
2022-05-07 22:09:59 +0800

    initial commit

---
+++ chatbot/chat.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from flask import render_template, url_for, redirect, Blueprint, abort, request
+
+from flask_login import login_required, current_user
+
+from chatbot.models import db, Chat
+
+bp = Blueprint('chat', __name__)
+
+@bp.route('/health')
+def heath():
+    return "Healthy"
+
+@bp.route('/')
+@login_required
+def index():
+   
+    chats = []
+    for chat in current_user.chats:
+        chats.append(chat)
+    
+    return render_template('chat/index.html', chats=chats)
+
+@bp.route('/chat', methods=('POST',))
+@login_required
+def create_chat():
+    text = request.form['text']
+    
+    if not text:
+        flash('Chat cannot be empty')
+        return redirect(url_for('.index'))
+    
+    chat = Chat(text=text, user=current_user, message_from_bot=False)
+    db.session.add(chat)
+    db.session.commit()
+
+    chat_ans = Chat(text="Good question, I don't know", user=current_user, message_from_bot=True)
+    db.session.add(chat_ans)
+    db.session.commit()
+    
+    return redirect(url_for('.index'))
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from chatbot import create_app
+
+app = create_app()
